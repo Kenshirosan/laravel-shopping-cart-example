d71d0292bd12327002719c64eaeede7ffc1ddb40
laurent <laurent.sama@gmail.com>
2018-02-09 16:31:36 +0100

    small fix

--- resources/views/javascript/updateCart.blade.php
+++ resources/views/javascript/updateCart.blade.php
@@ -1,13 +1,13 @@
 <script>
     (function() {
         $('.quantity').on('change', function() {
-            let id = $(this).attr('data-id');
-            let url = '{{ url("/cart") }}' + '/' + id;
-            let data = {'quantity': this.value};
+            let id = $(this).attr('data-id'),
+                url = '{{ url("/cart") }}' + '/' + id,
+                data = {'quantity': this.value};
 
-            axios.patch(url, data).then(flash('Successfully updated !'))
-                                    .then(location.reload())
-                                    .catch(e => console.log(e));
+            axios.patch(url, data)
+                .then(window.location.href = '{{ url('/cart') }}')
+                .catch(e => console.log(e));
         });
     })();
 </script>
