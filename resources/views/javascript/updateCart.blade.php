6549b184eea13437685c7d0ab1cb59c073b375b2
Laurent <laurent.sama@gmail.com>
2018-04-06 15:49:41 -0400

    Typo

--- resources/views/javascript/updateCart.blade.php
+++ resources/views/javascript/updateCart.blade.php
@@ -2,15 +2,15 @@
     (function() {
         $('.quantity').on('change', function() {
             let id = $(this).attr('data-id'),
-                url = '{{ url("/cart") }}' + '/' + id,
+                url = '/cart/' + id,
                 data = {'quantity': this.value};
 
             axios.patch(url, data)
                 .then(flash('Successfully updated !'))
                 .then(setTimeout( () => {
                     location.reload()
-                }, 100))
-                .catch(e => flash('Something wrong happened, try again later', 'danger'));
+                }, 500))
+                .catch(e => flash('Something wrong happened, try again later ' + e, 'danger'));
         });
     })();
 </script>
