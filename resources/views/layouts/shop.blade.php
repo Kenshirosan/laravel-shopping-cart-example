b6ea3bea613dc998a17cfb0d87971cda26b841b2
Laurent <laurent.sama@gmail.com>
2018-04-07 13:19:07 -0400

    Small cleaning

--- resources/views/layouts/shop.blade.php
+++ resources/views/layouts/shop.blade.php
@@ -6,7 +6,11 @@
 
 @section('content')
     <noscript>
-     <h1 style="color:red">Javascript must be enabled past this point, please check how to activate javascript for your browser</h1>
+        <div class="row">
+            <div class="alert alert-danger">
+                <h1>Javascript must be enabled to shop on this website, please check how to activate javascript for your browser</h1>
+            </div>
+        </div>
     </noscript>
 
     <section class="shopping">
@@ -15,7 +19,9 @@
             <p class="custom-subtitle">Place your order !</p>
         </div>
         @foreach ($categories as $category)
-            @if( ! $category->products->isEmpty())
+            @if($category->products->isEmpty())
+                <h1 class="text-info">Construction Site, Please check back later.</h1>
+            @else
                 <div class="row">
                     <div class="well text-center">
                         <h1>{{ $category->name }}</h1>
