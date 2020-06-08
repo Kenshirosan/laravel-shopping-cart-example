4d76dfc6fab65ed89055da97697fdeb84eb04c11
laurent <l.neveux@icloud.com>
2020-06-08 19:21:19 +0200

    card styling

--- resources/views/includes/alt-card.blade.php
+++ resources/views/includes/alt-card.blade.php
@@ -41,7 +41,8 @@
 {{--            @endif--}}
         </div>
     </div>
-    <div class="cart-btn-container">
+    <div role="separator"></div>
+        <div class="cart-btn-container">
         <favorite class="cart-btn" :product="{{ $product }}"></favorite>
         @if($_SERVER['REQUEST_URI'] != "/shop/$product->slug")
             <a class="cart-btn" href="/shop/{{ $product->slug }}">Voir</a>
